d3708d801778acd26181c5ebcf2b1736da9b6b02
AlexUva19 <alexander.uitendaal@gmail.com>
2020-01-16 16:10:02 +0100

    Ik heb exit points toegevoegd aan de map,

--- src/resources/create_map_concertgebouw.py
+++ src/resources/create_map_concertgebouw.py
@@ -64,8 +64,42 @@ f.close()
 
 
 mazeGK = load_map_from_file("concertgebouwmap.txt")
-print(mazeGK)
+
+
+# Define exit point ; garderobe, 6 ingangen
+
+garderobe_1 = Point(23,12)
+garderobe_2 = Point(23,23)
+garderobe_3 = Point(23,34)
+garderobe_4 = Point(23,45)
+garderobe_5 = Point(23,57)
+
+# aan de zijkant zaal bij binnekomst
+exit_1 = Point(37,6)
+exit_2 = Point(81,6)
+
+# aan de zijkant zaal tegenover concertzaal bij binnekomst
+exit_3 = Point(37,95)
+exit_4 = Point(81,95)
+
+# aan de voorkant van het gebouw
+
+exit_5 = Point(95, 20)
+exit_6 = Point(95, 34)
+exit_7 = Point(95, 49)
+
+# garderobe exits
+
+exits= [garderobe_1,garderobe_2,garderobe_3,garderobe_4,garderobe_5]
+
+# deur exits
+
+# exits = [exit_1, exit_2, exit_3, exit_4, exit_5, exit_6, exit_7]
+
+directions = direction_map(mazeGK, exits, 1)
+
 # directions = direction_map(mazeGK, exits, 1)
+
 
 # load the text file
 
